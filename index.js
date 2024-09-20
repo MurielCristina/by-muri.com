776930476581ac77a116c4b8e9374388a9a691af
Muri <mcsanchezlaguna@gmail.com>
2024-09-20 17:13:33 +0200

    fix toggle theme bugg1

--- index.js
+++ index.js
@@ -5,17 +5,32 @@ let topBar = document.getElementById("top-bar");
 let bigFooter = document.getElementById("big-footer");
 
 themeButton.addEventListener("click", () => {
-  body.classList.toggle("light-theme");
-  body.classList.toggle("dark-theme");
-  topBar.classList.toggle("dark-theme");
-  topBar.classList.toggle("light-theme");
-  bigFooter.classList.toggle("dark-theme");
-  bigFooter.classList.toggle("light-theme");
-
   if (body.classList.contains("dark-theme")) {
-    themeButton.textContent = "Switch to Light";
+    body.classList.remove("dark-theme");
+    body.classList.add("light-theme");
+
+    topBar.classList.remove("light-theme");
+    topBar.classList.add("dark-theme");
+
+    if (bigFooter) {
+      bigFooter.classList.remove("light-theme");
+      bigFooter.classList.add("dark-theme");
+    }
+
+    themeButton.textContent = "🌑";
   } else {
-    themeButton.textContent = "Switch to Dark";
+    body.classList.remove("light-theme");
+    body.classList.add("dark-theme");
+
+    topBar.classList.remove("dark-theme");
+    topBar.classList.add("light-theme");
+
+    if (bigFooter) {
+      bigFooter.classList.remove("dark-theme");
+      bigFooter.classList.add("light-theme");
+    }
+
+    themeButton.textContent = "☀️";
   }
 });
 
@@ -23,14 +38,18 @@ themeButton.addEventListener("click", () => {
 let mailButton = document.getElementById("mail");
 let phoneButton = document.getElementById("phone");
 
-mailButton.addEventListener("click", () => {
-  alert(
-    "I know how annoying it is when the mail app opens. Here’s my email for you to copy and paste: mcsanchezlaguna@gmail.com"
-  );
-});
+if (mailButton) {
+  mailButton.addEventListener("click", () => {
+    alert(
+      "I know how annoying it is when the mail app opens. Here’s my email for you to copy and paste: mcsanchezlaguna@gmail.com"
+    );
+  });
+}
 
-phoneButton.addEventListener("click", () => {
-  alert("Please call between 13-19 (GMT+2) - (🇪🇸)648440317");
-});
+if (phoneButton) {
+  phoneButton.addEventListener("click", () => {
+    alert("Please call between 13-19 (GMT+2) - (🇪🇸)648440317");
+  });
+}
 
 // typing effect in About page
