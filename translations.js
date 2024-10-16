b4a74ca64a45751a699900ae29d60e49684389fe
Muri <mcsanchezlaguna@gmail.com>
2024-10-16 11:16:27 +0200

    add translation.js for multi-language support

---
+++ translations.js
@@ -0,0 +1,118 @@
+const translations = {
+  en: {
+    navbar: {
+      home: "home",
+      projects: "projects",
+      about: "about",
+      contact: "contact",
+      themeToggle: "🌔",
+    },
+    heroHeader: {
+      greeting: "Hi there, I'm",
+      jobTitle: "Font-End Developer, based in 🇪🇺",
+      aboutMe: "About me",
+      contactMe: "Contact me",
+    },
+    about: {
+      title: "About",
+      introduction: "I'm Muri.",
+      description1: "👩🏽‍💻 A front-end developer",
+      description2: "👩🏽‍💼 A project manager",
+      description3: "👷🏽‍♀️ An electromechanical engineer",
+      fullName:
+        "Hello! I'm <strong>Muriel Sánchez Laguna</strong>, a front-end developer with a passion for continuous learning and creating seamless user experiences. As someone who believes in the saying,",
+      quote: "You are always a student, never a master",
+      fullDescription: `I'm constantly striving to improve my skills and explore new technologies. My journey into coding has been driven by a genuine curiosity for how digital products are built and a desire to contribute to impactful web projects. I thrive on turning ideas into reality through clean, efficient code, and I'm excited to continue growing as a front-end developer.<br/><br/>I originally studied electromechanical engineering, where I gained a strong technical foundation, and later completed a master's degree in project management. This combination of engineering and project management has equipped me with a unique skill set that I now bring to my front-end work. My experience in managing complex projects has helped me develop a strategic mindset, ensuring that I not only focus on delivering great user experiences but also on meeting project goals efficiently and effectively.<br/><br/>As a Panamanian professional, I'm eager to leverage my diverse background in remote front-end roles, contributing to innovative digital projects. I’m excited by the challenges that come with building responsive, user-friendly applications, and I look forward to collaborating on dynamic projects where I can apply my technical skills, creativity, and love for learning.`,
+      contactButton: "CONTACT ME",
+      cvEnglish: "CV-English",
+      cvSpanish: "CV-Español",
+      skills: "Skills",
+      tools: "Tools",
+      toolsSkills:
+        "HTML, CSS, JavaScript, React.js, Bootstrap, Responsive development, API, Hosting, AI, SEO",
+      toolsList: "VS Code, MacOS, Git, GitHub",
+    },
+    contact: {
+      title: "Contact me",
+      description:
+        "Need Front-End developer to help out? Got a question, proposal or project or want to work together on something? Feel free to reach out.",
+      seeProfile: "You can also see my profile in:",
+      openSource:
+        "This page was coded by Muri and it's open-source at GitHub and hosted at Netlify",
+      opportunities: "Open for oportunities? YES",
+    },
+    projects: {
+      title: "Personal Projects",
+      description:
+        "This is a collection of my favorite projects—each one marks a step in my journey as a developer. The world of digital development is always changing, and so am I. Every day, I’m learning something new, refining my skills, and enjoying the process of growth along the way.",
+      moreProjectsButton: "More projects",
+      vanillaTitle: "Built in Vanilla",
+      vanillaDescription:
+        "In the culinary world, vanilla often connotes simplicity and unadorned flavor. In web development, 'vanilla' carries a similar meaning, denoting the use of pure, framework-free coding languages such as HTML, CSS, and JavaScript. I enjoy the challenge and freedom that comes with writing in vanilla, crafting projects from the ground up without relying on external libraries. It allows me to maintain full control over every line of code, ensuring performance and precision in every detail.",
+      reactTitle: "Built in React.js",
+      reactDescription:
+        "React.js brings a dynamic, component-based approach to building user interfaces. I love working with React because it allows me to create fast, scalable, and interactive applications with ease.",
+      othersTitle: "Others",
+      othersDescription:
+        "This section showcases projects built using diverse frameworks and technologies, like Python. I enjoy exploring different tools to expand my development skills, creating versatile and efficient web solutions beyond the familiar stack.",
+    },
+  },
+  es: {
+    navbar: {
+      home: "inicio",
+      projects: "proyectos",
+      about: "acerca de",
+      contact: "contacto",
+      themeToggle: "🌒",
+    },
+    heroHeader: {
+      greeting: "Hola, soy",
+      jobTitle: "Desarrolladora Front-End, basada en 🇪🇺",
+      aboutMe: "Acerca de mí",
+      contactMe: "Contáctame",
+    },
+    about: {
+      title: "Acerca de",
+      introduction: "Soy Muri.",
+      description1: "👩🏽‍💻 Desarrolladora front-end",
+      description2: "👩🏽‍💼 Gerente de proyectos",
+      description3: "👷🏽‍♀️ Ingeniera electromecánica",
+      fullName:
+        "¡Hola! Soy <strong>Muriel Sánchez Laguna</strong>, desarrolladora front-end con pasión por el aprendizaje continuo y la creación de experiencias de usuario fluidas. Como alguien que cree en la frase,",
+      quote: "Siempre estudiante, nunca maestro",
+      fullDescription: `Siempre busco mejorar mis habilidades y explorar nuevas tecnologías. Mi viaje en el mundo de la programación ha sido impulsado por una curiosidad genuina por cómo se construyen los productos digitales y el deseo de contribuir a proyectos web impactantes. Me encanta transformar ideas en realidad mediante código limpio y eficiente, y estoy emocionada por seguir creciendo como desarrolladora front-end.<br/><br/>Originalmente estudié ingeniería electromecánica, donde obtuve una base técnica sólida, y más tarde completé una maestría en gestión de proyectos. Esta combinación de ingeniería y gestión de proyectos me ha brindado un conjunto de habilidades únicas que ahora aplico en mi trabajo de front-end. Mi experiencia en la gestión de proyectos complejos me ha ayudado a desarrollar una mentalidad estratégica, asegurando que no solo me enfoque en ofrecer grandes experiencias de usuario, sino también en cumplir objetivos de proyecto de manera eficiente y efectiva.<br/><br/>Como profesional panameña, estoy ansiosa por aprovechar mi diversa experiencia en roles remotos de front-end, contribuyendo a proyectos digitales innovadores. Me emocionan los desafíos que conlleva construir aplicaciones responsivas y fáciles de usar, y espero colaborar en proyectos dinámicos donde pueda aplicar mis habilidades técnicas, creatividad y amor por el aprendizaje.`,
+      contactButton: "CONTÁCTAME",
+      cvEnglish: "CV-Inglés",
+      cvSpanish: "CV-Español",
+      skills: "Habilidades",
+      tools: "Herramientas",
+      toolsSkills:
+        "HTML, CSS, JavaScript, React.js, Bootstrap, Desarrollo Responsive, API, Hosting, IA, SEO",
+      toolsList: "VS Code, MacOS, Git, GitHub",
+    },
+    contact: {
+      title: "Contáctame",
+      description:
+        "¿Necesitas una desarrolladora front-end para ayudar? ¿Tienes una pregunta, propuesta o proyecto, o quieres colaborar en algo? No dudes en comunicarte.",
+      seeProfile: "También puedes ver mi perfil en:",
+      openSource:
+        "Esta página fue codificada por Muri y es de código abierto en GitHub y está alojada en Netlify",
+      opportunities: "¿Abierta a oportunidades? SÍ",
+    },
+    projects: {
+      title: "Proyectos Personales",
+      description:
+        "Esta es una colección de mis proyectos favoritos; cada uno marca un paso en mi viaje como desarrolladora. El mundo del desarrollo digital está en constante cambio, y yo también. Cada día aprendo algo nuevo, perfecciono mis habilidades y disfruto el proceso de crecimiento a lo largo del camino.",
+      moreProjectsButton: "Más proyectos",
+      vanillaTitle: "Hechos en Vanilla",
+      vanillaDescription:
+        "En el mundo culinario, la vainilla a menudo connota simplicidad y sabor sin adornos. En el desarrollo web, 'vanilla' tiene un significado similar, denotando el uso de lenguajes de codificación puros, sin frameworks, como HTML, CSS y JavaScript. Disfruto el desafío y la libertad que conlleva escribir en vanilla, creando proyectos desde cero sin depender de librerías externas. Esto me permite tener control total sobre cada línea de código, asegurando rendimiento y precisión en cada detalle.",
+      reactTitle: "Hechos en React.js",
+      reactDescription:
+        "React.js aporta un enfoque dinámico y basado en componentes para construir interfaces de usuario. Me encanta trabajar con React porque me permite crear aplicaciones rápidas, escalables e interactivas con facilidad.",
+      othersTitle: "Otros",
+      othersDescription:
+        "Esta sección muestra proyectos construidos utilizando diferentes frameworks y tecnologías, como Python. Me gusta explorar diferentes herramientas para expandir mis habilidades de desarrollo, creando soluciones web versátiles y eficientes más allá de la pila habitual.",
+    },
+  },
+};
